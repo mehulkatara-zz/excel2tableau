68887b11f652d10669d242807e309f2a9e3cf5a5
Mehul Katara <mehulkatara@hotmail.com>
2017-12-12 15:35:04 +0530

    Update batch.R
    export in csv

--- batch.R
+++ batch.R
@@ -1,102 +1,7 @@
 #file1#
+
 library(xlsx)
 library(tcltk)
-data=read.xlsx("T:\\Tableau_BankProject\\Advanced Modules Implementation Plan vs Status.xlsx" , sheetIndex = 2 , header = FALSE)
-Data_Frame = data.frame(data)
-Data_Frame <- Data_Frame[-3,-1]
-Data_Frame=Data_Frame[-1,]
-Data_Frame[] <- lapply(Data_Frame, as.character)
-colnames(Data_Frame) <- Data_Frame[1,]
-
-
-
-rowlength=length((Data_Frame[1,]))
-droprow=max(which(Data_Frame[1,] =="BANKPERSON "))
-
-Data_Frame= Data_Frame[-1,-droprow:-rowlength]
-
-filelength=length(which(Data_Frame[,1] !="NA"))
-
-
-Data_Frame= Data_Frame[1:filelength,]
-
-
-Total_Products= length(Data_Frame[3:length(Data_Frame[1,])])
-
-
-Total_Banks= length(Data_Frame[,1])
-
-
-product=colnames(Data_Frame)
-product=product[3:length(product)]
-bank=Data_Frame[,1]
-
-
-
-new=data.frame()
-
-b=1
-b1=1
-p1=1
-for (p1 in 1:Total_Banks) 
-{
-  for (b in 1:Total_Products) 
-  {
-    new[b1,1]= bank[p1]
-    b1=b1+1
-  }
-  p1=p1+1
-  
-}
-
-b=1
-b1=1
-p1=1
-p2=1
-for (p1 in 1:Total_Banks) 
-{
-  for (b in 1:Total_Products) 
-  {
-    new[b1,2]= product[p2]
-    b1=b1+1
-    p2=p2+1
-  }
-  b=1
-  p2=1
-  
-  
-  
-  p1=p1+1}
-
-
-b=1
-b1=1
-p1=1
-n=1
-m=3
-for (p1 in 1:Total_Banks) 
-{
-  for (b in 1:Total_Products) 
-  {
-    new[b1,3]= Data_Frame[n,m]
-    b1=b1+1
-    m=m+1
-    
-  }
-  b=1
-  m=3
-  n=n+1
-  
-  p1=p1+1}
-
-
-names(new)[1]<-paste("Bank")
-names(new)[2]<-paste("Product")
-names(new)[3]<-paste("Status")
-write.xlsx(x = new , file = "T:\\Tableau_BankProject\\AdvancedModulesTBA.xlsx",sheetName = "TestSheet",row.names=FALSE)
-
-#file2#
-
 data=read.xlsx("T:\\Tableau_BankProject\\Advanced Modules Implementation Plan vs Status.xlsx" , sheetIndex = 1 , header = FALSE)
 Data_Frame = data.frame(data)
 Data_Frame <- Data_Frame[-3,-1]
@@ -189,7 +94,105 @@ for (p1 in 1:Total_Banks)
 names(new)[1]<-paste("Bank")
 names(new)[2]<-paste("Product")
 names(new)[3]<-paste("Status")
-write.xlsx(x = new , file = "T:\\Tableau_BankProject\\AdvancedModulesASP.xlsx",sheetName = "TestSheet",row.names=FALSE)
+
+write.csv(x = new , file = "T:\\Tableau_BankProject\\TableauAdvancedModulesASP.csv",row.names=FALSE)
+
+#file2#
+
+data=read.xlsx("T:\\Tableau_BankProject\\Advanced Modules Implementation Plan vs Status.xlsx" , sheetIndex = 2 , header = FALSE)
+Data_Frame = data.frame(data)
+Data_Frame <- Data_Frame[-3,-1]
+Data_Frame=Data_Frame[-1,]
+Data_Frame[] <- lapply(Data_Frame, as.character)
+colnames(Data_Frame) <- Data_Frame[1,]
+
+
+
+rowlength=length((Data_Frame[1,]))
+droprow=max(which(Data_Frame[1,] =="BANKPERSON "))
+
+Data_Frame= Data_Frame[-1,-droprow:-rowlength]
+
+filelength=length(which(Data_Frame[,1] !="NA"))
+
+
+Data_Frame= Data_Frame[1:filelength,]
+
+
+Total_Products= length(Data_Frame[3:length(Data_Frame[1,])])
+
+
+Total_Banks= length(Data_Frame[,1])
+
+
+product=colnames(Data_Frame)
+product=product[3:length(product)]
+bank=Data_Frame[,1]
+
+
+
+new=data.frame()
+
+b=1
+b1=1
+p1=1
+for (p1 in 1:Total_Banks) 
+{
+  for (b in 1:Total_Products) 
+  {
+    new[b1,1]= bank[p1]
+    b1=b1+1
+  }
+  p1=p1+1
+  
+}
+
+b=1
+b1=1
+p1=1
+p2=1
+for (p1 in 1:Total_Banks) 
+{
+  for (b in 1:Total_Products) 
+  {
+    new[b1,2]= product[p2]
+    b1=b1+1
+    p2=p2+1
+  }
+  b=1
+  p2=1
+  
+  
+  
+  p1=p1+1}
+
+
+b=1
+b1=1
+p1=1
+n=1
+m=3
+for (p1 in 1:Total_Banks) 
+{
+  for (b in 1:Total_Products) 
+  {
+    new[b1,3]= Data_Frame[n,m]
+    b1=b1+1
+    m=m+1
+    
+  }
+  b=1
+  m=3
+  n=n+1
+  
+  p1=p1+1}
+
+
+names(new)[1]<-paste("Bank")
+names(new)[2]<-paste("Product")
+names(new)[3]<-paste("Status")
+
+write.csv(x = new , file = "T:\\Tableau_BankProject\\TableauAdvancedModulesTBA.csv",row.names=FALSE)
 
 #file3#
 
@@ -249,7 +252,7 @@ for(p1 in 1:length(Data_Frame[,1]))
 names(new)[1]<-paste("Date")
 names(new)[2]<-paste("Category")
 names(new)[3]<-paste("Count")
-write.xlsx(x = new , file = "T:\\Tableau_BankProject\\Summary of QA In (QA Allotted).xlsx",sheetName = "Summary of QA In (QA Allotted)",row.names=FALSE)
+write.csv(x = new , file = "T:\\Tableau_BankProject\\TableauQAAllotted.csv",row.names=FALSE)
 
 #file4#
 
@@ -309,7 +312,7 @@ for(p1 in 1:length(Data_Frame[,1]))
 names(new)[1]<-paste("Date")
 names(new)[2]<-paste("Category")
 names(new)[3]<-paste("Count")
-write.xlsx(x = new , file = "T:\\Tableau_BankProject\\Summary of QA Out (QA Release).xlsx",sheetName = "Summary of QA Out (QA Release)",row.names=FALSE)
+write.csv(x = new , file = "T:\\Tableau_BankProject\\TableauQARelease.csv",row.names=FALSE)
 
 #File5#
 
@@ -317,6 +320,7 @@ data=read.xlsx("T:\\Tableau_BankProject\\Support App Usage.xlsx", sheetIndex = 1
 Data_Frame = data.frame(data)
 Data_Frame[] <- lapply(Data_Frame, as.character)
 category= colnames(Data_Frame)
+category=category[-6]
 category=category[-5]
 category=category[-1]
 new= data.frame()
@@ -373,7 +377,7 @@ names(new)[2]<-paste("Category")
 names(new)[3]<-paste("Count")
 names(new)[4]<-paste("Total Bank")
 
-write.xlsx(x = new , file = "T:\\Tableau_BankProject\\SupportAppUsage.xlsx",sheetName = "Category wise support app usage",row.names=FALSE)
+write.csv(x = new , file = "T:\\Tableau_BankProject\\TableauSupportAppUsage.csv",row.names=FALSE)
 
 
 msgBox <- tkmessageBox(title = "Hello, Tableau Developer",
