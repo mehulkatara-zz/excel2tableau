1386058e8a9a7a47afaa2b09a50a0fec53e60f6e
Mehul Katara <mehulkatara@hotmail.com>
2017-12-18 17:18:17 +0530

    autoupdate

---
+++ autoupdate.R
@@ -0,0 +1,12 @@
+library(readr)
+update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
+url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
+file=basename(url)
+
+if (update[1,2]==0)
+{
+download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
+source(file)
+}
+
+{source(file)}
