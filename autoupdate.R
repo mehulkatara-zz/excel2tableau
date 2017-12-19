e94dcaed99cb5852618e1db7c2b22ad0c77f838e
Mehul Katara <mehulkatara@hotmail.com>
2017-12-19 16:14:12 +0530

    logic updated

--- autoupdate.R
+++ autoupdate.R
@@ -1,9 +1,10 @@
+setwd("T:/Repository/Mehul/excel2tableau")
 library(readr)
 update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
 url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
 file=basename(url)
 
-if (update[1,2]==0)
+if (update[1,2]==1)
 {
 download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
 source(file)
