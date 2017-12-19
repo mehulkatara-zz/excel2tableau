88b98b39eb216d6bc515c434fd8caf3fcb9285e8
Mehul Katara <mehulkatara@hotmail.com>
2017-12-19 17:11:08 +0530

    selfupdate code when internet is active

--- autoupdate.R
+++ autoupdate.R
@@ -1,13 +1,27 @@
 setwd("T:/Repository/Mehul/excel2tableau")
 library(readr)
-update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
-url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
-file=basename(url)
-
-if (update[1,2]==1)
+library(pingr)
+if (pingr::is_online()==TRUE)
 {
-download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
-source(file)
+  update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
+  url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
+  file=basename(url)
+  
+  if (update[1,2]==1)
+  {
+    download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
+    source(file)
+  }
+  
+  else
+  
+  {
+    source(file)
+  }
+}else
+{
+  source(file)
 }
 
-{source(file)}
+
+
