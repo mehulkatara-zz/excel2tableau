22765e15854fbf93fcea41858aa2ca02192f4215
Mehul Katara <mehulkatara@hotmail.com>
2017-12-20 12:15:33 +0530

    auto updated file fixed when there is no internet

--- autoupdate.R
+++ autoupdate.R
@@ -1,27 +1,12 @@
 setwd("T:/Repository/Mehul/excel2tableau")
 library(readr)
-library(pingr)
-if (pingr::is_online()==TRUE)
+file="batch.R"
+source(file)
+update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
+url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
+file=basename(url)
+if (update[1,2]==1)
 {
-  update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
-  url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
-  file=basename(url)
-  
-  if (update[1,2]==1)
-  {
-    download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
-    source(file)
-  }
-  
-  else
-  
-  {
-    source(file)
-  }
-}else
-{
+  download.file(url,file,quiet = FALSE, mode = "w", cacheOK = TRUE)
   source(file)
 }
-
-
-
