0dc0d9060d0575f4de5b9dc4e0c0e0a02d482e2b
Mehul Katara <kataram1@DMICorp.com>
2017-12-21 02:50:58 -0600

    internet checking logic updated

--- autoupdate.R
+++ autoupdate.R
@@ -1,7 +1,19 @@
 setwd("T:/Repository/Mehul/excel2tableau")
 library(readr)
 library(pingr)
-if (pingr::is_online()==TRUE)
+can_internet <- function(url = "http://www.google.com") {
+  
+  # test the http capabilities of the current R build
+  if (!capabilities(what = "http/ftp")) return(FALSE)
+  
+  # test connection by trying to read first line of url
+  test <- try(suppressWarnings(readLines(url, n = 1)), silent = TRUE)
+  
+  # return FALSE if test inherits 'try-error' class
+  !inherits(test, "try-error")
+}
+
+if (can_internet()==TRUE)
 {
   update <- read_csv("https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/update.csv",col_names = FALSE)
   url="https://raw.githubusercontent.com/mehulkatara/excel2tableau/master/batch.R"
