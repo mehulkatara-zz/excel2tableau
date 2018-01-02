96380de9dd00c27c9754ec8ec9a8fabb57101071
Mehul Katara <mehul.katara@tmspl.com>
2018-01-02 18:01:10 +0530

    time date fixed

--- jsontomysql.R
+++ jsontomysql.R
@@ -1,21 +1,44 @@
+#setting working directory 
 setwd("T:\\Tableau_BankProject")
 
+#link of the json file
 url="http://srvmanager.tmsys.in:81/ServerManagerTmspl/dashboardtm/getDataApi"
 
+#download json file
 download.file(url,"datacenter.json",quiet = TRUE, mode = "w")
 
 library(jsonlite)
 
+#reading json file
 mydf = fromJSON("datacenter.json",simplifyVector = TRUE)
 
+# removeing unnecessary data and store to dataframe
 mydf = mydf[["server_data"]]
+
+#Replace N/A with blank
+mydf[] <- lapply(mydf[], gsub, pattern = 'N/A', replacement='')
+
+#Replace space with :
+mydf[5]=lapply(mydf[5],gsub, pattern=' ', replacement = ':')
+
+#Replace alpha with blank
+mydf[5]=lapply(mydf[5],gsub, pattern='[[:alpha:]]', replacement = '')
+
+#Get current date
+date=format(Sys.time(), "%Y-%m-%d")
+
+#Current date store in dataframe
+mydf[5]=lapply(mydf[5],function(x) paste(date, x,sep=" "))
+
 
 library(RMySQL)
 
 mydb = dbConnect(MySQL(), user='tmspl', password='tmsystem@321', dbname='ServerDashboard', host='192.168.52.208')
 
+#delete existed table
 dbGetQuery(mydb, "DROP TABLE `server_info`;")
 
+#Create database table
 dbGetQuery(mydb, "CREATE TABLE `server_info` (
   `row_names` int(225) DEFAULT NULL,
            `id` int(225) DEFAULT NULL,
@@ -29,6 +52,8 @@ dbGetQuery(mydb, "CREATE TABLE `server_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
            ")
 
+#saving dataframe to mysql
 dbWriteTable(mydb, name='server_info', value=mydf,append=TRUE)
 
+#closeing database connection
 dbDisconnect(mydb)
