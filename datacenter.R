e015403a7ba731477acb4d3b7db171f606f6a5f6
Mehul Katara <mehul.katara@tmspl.com>
2018-01-06 17:35:49 +0530

    Data Storage change to mysql

--- datacenter.R
+++ datacenter.R
@@ -1,12 +1,15 @@
+library(RMySQL)
+mydb = dbConnect(MySQL(), user='tmspl', password='tmsystem@321', dbname='ServerDashboard', host='192.168.52.208')
+library(reshape2)
 setfolder="T:\\Tableau_BankProject\\email"
 dir=list.dirs(setfolder ,recursive = FALSE) #find all folder in directory
 dir <- dir[!grepl("[_]", basename(dir))]
 cat(paste0("\n Setting directory to "),setfolder)
-Sys.sleep(1)
+
 cat("\n Scaning folder and files")
-Sys.sleep(1)
+
 cat(paste0("\n Loading ",length(dir)," folder and ",length(list.files(dir))," Files"))
-Sys.sleep(1)
+
 check=length(dir)
 sleep=5
 if (check>0)
@@ -26,7 +29,7 @@ for(j in 1: dirnum)#loop for folder
   
       d <- stats:::format.ftable(d, quote=FALSE) #format files
       
-      d[,2]=d[1,2] # saving date in every coloum
+      d[,2] = d[1,2] # saving date in every coloum
       datacenter=substr(file[i],0,4)
       d=cbind(d,datacenter)
       
@@ -36,10 +39,25 @@ for(j in 1: dirnum)#loop for folder
       d<-gsub("\\s+", ";", gsub("^\\s+|\\s+$", "",d)) # remove space
       d<- gsub('\\s+',',',d) # replacing with ','
       
-      write.table(d,file="T:\\Tableau_BankProject\\all.csv",append = TRUE,col.names=FALSE) # save and apped in csv
+      d=as.data.frame(d)
+      d <- data.frame(d, colsplit(d$V1,pattern = ";",names=c("Bank User","Space Occupied(GB)")))
+      
+      drop <- c("V1")
+      d=d[ , !(names(d) %in% drop)]
+      
+      names(d)[1]=paste("Date")
+      names(d)[2]=paste("Data Center")
+      names(d)[3]=paste("Bank User")
+      names(d)[4]=paste("Space Occupied(GB)")
+      d$Date=as.Date(d$Date,format = "%d-%b-%y")
+      
+      #write.table(d,file="T:\\Tableau_BankProject\\all.csv",append = TRUE,col.names=FALSE,sep = ",",row.names = FALSE) # save and apped in csv
+      
+      #saving dataframe to mysql
+      dbWriteTable(mydb, name='SpaceUtilization', value=d,append=TRUE,row.names = FALSE)
       
       
-	    cat(paste0("\n ",allfiles," ","Files remaing to append"))
+      cat(paste0("\n ",allfiles," ","Files remaing to append"))
 	    allfiles=allfiles-1
       i = i + 1
     }
@@ -50,14 +68,7 @@ for(j in 1: dirnum)#loop for folder
   
   j = j + 1
 }
-cat("\n Successfully Append")
-cat(paste0("\n Date saved in ",setfolder,"/all.csv"))
-Sys.sleep(sleep)
-sleep=0
+
 }
-{cat("\n None of files has been updated \n No folder found with new data")
-cat(paste0("\n Date saved in ",setfolder,"/all.csv"))
-}
-Sys.sleep(sleep)
-#library(reshape2)
-#test <- data.frame(df, colsplit(df$VESSELID,pattern = ":",names=c("vesselID","DATE")))
+
+dbDisconnect(mydb)
